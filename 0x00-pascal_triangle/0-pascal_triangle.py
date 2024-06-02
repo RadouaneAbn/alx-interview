c688d60d4b8ffd7136fd73a533061bdd6510159e
RadouaneAbn <radouaneabn@gmail.com>
2024-06-02 23:24:36 +0100

    Pascal's triangle - add documentation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
+"""
+Pascal's triangle
+"""
 
-def pascal_triangle(num):
-    if type(num) is not int:
+
+def pascal_triangle(n):
+    """ This function return a list of lists representing
+        Pascla's triangle of a number n
+    """
+    if type(n) is not int:
         raise TypeError("The input should be a number")
-    if num <= 0:
+    if n <= 0:
         return []
     result = [[1]]
-    for i in range(1, num):
+    for i in range(1, n):
         prev = i - 1
-        result.append([(j > 0 and result[prev][j - 1]) + (j < i and result[prev][j])
-                       for j in range(0, i + 1)
+        result.append([
+            (j > 0 and result[prev][j - 1]) + (j < i and result[prev][j])
+            for j in range(0, i + 1)
             ])
 
     return result
