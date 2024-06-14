1b0c7a27f3b173446f1c2d52d7714dc27d339004
RadouaneAbn <radouaneabn@gmail.com>
2024-06-14 03:22:23 +0100

    Task 0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Minimum Operations """
+
+from math import sqrt, floor
+
+
+def minOperations(n: int) -> int:
+    """ This function return the number of minimum operations
+        needed to result in exactly n H.
+    """
+    if n == 2:
+        return 2
+    elif n == 1:
+        return 0
+
+    result = []
+    n_prev = n
+    n_sqrt = floor(sqrt(n))
+    while True:
+        trigger = False
+        for i in range(2, n_sqrt + 1):
+            if n_prev % i == 0:
+                trigger = True
+                result.append(i)
+                n_prev = int(n_prev / i)
+                break
+        if not trigger:
+            break
+
+    if len(result) == 0:
+        return 0
+
+    return sum(result)
