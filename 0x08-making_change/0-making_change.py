124b23d7652b307c28efbe90ebf3ad5a5d677966
RadouaneAbn <radouaneabn@gmail.com>
2024-08-21 01:28:03 +0100

    Task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,11 +5,15 @@
 def makeChange(coins, total):
     """ This function determines the fewest number of coins needed to meet
         a given amount 'total' """
+    if not coins or coins is None:
+        return -1
+    if total <= 0:
+        return 0
     coins = list(sorted(coins, reverse=True))
     coins_count = 0
     number_of_coins = len(coins)
     i = 0
-    while coins and i < number_of_coins:
+    while i < number_of_coins:
         coin = coins[i]
         if coin > total or coin == 0:
             i += 1
