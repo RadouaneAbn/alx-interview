40b4ee21342dc5a5d7e61442686cc073ce2e1479
RadouaneAbn <radouaneabn@gmail.com>
2024-09-05 22:04:13 +0100

    The prime game - new methos

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,58 +1,35 @@
 #!/usr/bin/python3
-""" The Prime Game Module
-"""
-
+"""Module for prime game"""
 
 def is_multiple(n, m):
     """ This function return true if n is not a multiple of m """
     return n % m != 0
 
-
-def is_prime(n):
-    """ This function checks if number is prime"""
-    if n == 1:
-        return False
-    i = 2
-    while i * i <= n:
-        if n % i == 0:
-            return False
-        i += 1
-    return True
-
-
-def get_prime(nums):
-    """ This function return a prime number from a list if found """
-    for n in nums:
-        if is_prime(n):
-            return (n)
-    return None
+def get_all_primes(n):
+    """this function returns the number of all prime numbers from 2 to n """
+    all_nums = [p for p in range(2, n + 1)]
+    prim_count = 0
+    while all_nums:
+        n = all_nums[0]
+        prim_count += 0
+        all_nums = list(filter(lambda x: is_multiple(x, n), all_nums))
+    return prim_count
 
 
 def isWinner(x, nums):
-    """ This function gets teh winner of the prime game game """
+    """This function gets teh winner of the prime game game"""
     if x is None or nums is None or x == 0 or nums == []:
         return None
-    score = {"maria": 0, "ben": 0}
-    i = 0
-    while i < x:
-        new_list = [n for n in range(1, nums[i] + 1)]
-        turn = "maria"
-        while new_list:
-            # print("before:", new_list)
-            n = get_prime(new_list)
-            turn = "ben" if turn == "maria" else "maria"
-            if not n:
-                break
-            # print("prime found:", n)
-            new_list = list(filter(lambda x: is_multiple(x, n), new_list))
-            # print("after:", new_list)
-        # turn = "ben" if turn == "maria" else "maria"
-        score[turn] += 1
-        # print(score)
-        i += 1
-    if score["ben"] > score["maria"]:
-        return "ben"
-    elif score["maria"] > score["ben"]:
-        return "maria"
-    else:
-        return None
+    Maria = 0
+    Ben = 0
+    for i in range(x):
+        prime_count = get_all_primes(nums[i])
+        if prime_count % 2 == 0:
+            Ben += 1
+        else:
+            Maria += 1
+    if Maria > Ben:
+        return 'Maria'
+    elif Ben > Maria:
+        return 'Ben'
+    return None
